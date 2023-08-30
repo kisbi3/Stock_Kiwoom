3b449993896abb9d41add21c38b54f8b61812b6b
Jaesung_Kim <kisbi3@naver.com>
2023-08-30 23:18:26 +0900

    BasicLecture27

--- Qthread_2.py
+++ Qthread_2.py
@@ -34,3 +34,30 @@ class Thread2(QThread):
             code_list.append(code)
         
         print("계좌 종목 개수 %s" % (code_list))
+
+        # self.parent.progressBar5.setMaximum(len(code_list) - 1 )      # 나중에 설명
+
+        for idx, code in enumerate(code_list):                          # code_list에 있는 종목번호들에 'index'부여해서 index는 idx, 종목번호는 code
+            # self.parent.progressBar5.setValue(idx)                    # 나중에 설명
+            
+            QTest.qWait(1000)       # 1000ms(1s)씩 지연 발생 -> 키움 서버에 짧은 시간 안에 너무 많은 명령을 전송하면 계좌가 일시 정지하게 됨.
+                                    # 보통 3.6s(3600ms)마다 1번씩 명령을 전송하는 것이 안전함.
+
+            self.k.kiwoom.dynamicCall("DisconnectRealData(QString)", self.Find_down_Screen)     # 해당 스크린을 끊고 다시 시작
+            # 몇개의 주문마다 스크린 번호를 바꿔 주거나 끊어줄지는 선택하자!
+
+            self.code_in_all = code                         # 종목코드 선언( 중간에 코드 정보 받아오기 위해서 ) -> 위험도 판단 시 사용될 예정
+            print("%s / %s  : 종목 검사 중 코드이름 : %s." % (idx + 1, len(code_list), self.code_in_all))
+
+            # 기관 평균가를 받아오는 날짜를 정의하는 곳
+            date_today = datetime.today().strftime("%Y%m%d")
+            date_prev = datetime.today() - timedelta(10)        # 넉넉히 1일 전의 데이터를 바당온다. 또는 20일 이상 데이터도 필요
+            date_prev = date_prev.strftime("%Y%m%d")
+
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "시작일자", date_prev)      # 모의투자 0000
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종료일자", date_today)
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "기관추정단가구분", "1")
+            self.k.kiwoom.dynamicCall("SetInputValue(QString, QString)", "외인추정단가구분", "1")
+            self.k.kiwoom.dynamicCall("CommRqData(String, String, int, String)", "종목별기관매매추이요청2", "opt10045", "0", self.Find_down_Screen)
+            self.detail_account_info_event_loop.exec_()
