1fd42eee72de977d7a1287aa6732842752676efa
Jaesung_Kim <kisbi3@naver.com>
2023-09-22 21:24:47 +0900

    BasicLecture43

--- Qthread_3.py
+++ Qthread_3.py
@@ -20,4 +20,37 @@ class Thread3(QThread):
         # 계좌번호 가져오는 부분은 Qthread_3 분리시 로그인 후 계좌번호를 가져오는 함수로 교체된다.
         # 각 Thread는 통신을 하지 못함 -> GUI에 입력된 계좌번호를 가져와야 함
 
+        self.Load_code()
+    
+    def Load_code(self):
+
+        if os.path.exists("dist/Selected_code.txt"):
+            f = open("dist/Selected_code.txt", "r", encoding = "utf8")
+            lines = f.readlines()                       # 여러 종목이 저장되어 있다면 모든 항목을 가져온다.
+            screen = 4000
+            
+            for line in lines:
+                if line != "":
+                    ls = line.split("\t")
+                    t_code = ls[0]
+                    t_name = ls[1]
+                    curren_price = ls[2]
+                    dept = ls[3]
+                    mesu = ls[4]
+                    n_o_stock = ls[5]
+                    profit =ls[6]
+                    loss = ls[7].split("\n")[0]
+
+                    self.k.portfolio_stock_dict.update({t_code: {"종목명": t_name}})        # dictionary에 종목 추가
+                    self.k.portfolio_stock_dict[t_code].update({"현재가": int(curren_price)})
+                    self.k.portfolio_stock_dict[t_code].update({"신용비율": dept})
+                    self.k.portfolio_stock_dict[t_code].update({"매수가": int(mesu)})
+                    self.k.portfolio_stock_dict[t_code].update({"매수수량": int(n_o_stock)})
+                    self.k.portfolio_stock_dict[t_code].update({"익절가": int(profit)})
+                    self.k.portfolio_stock_dict[t_code].update({"손절가": int(loss)})
+                    self.k.portfolio_stock_dict[t_code].update({"주문용스크린번호": screen})  # 아래 내용을 업데이트
+                    screen += 1                                                             # 종목마다 스크린 번호를 다르게 하기
+            
+            f.close()
+
 
