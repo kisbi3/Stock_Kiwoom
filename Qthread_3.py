baf8b1eb34c337d41edf3691e172401105ca4fea
Jaesung_Kim <kisbi3@naver.com>
2023-09-22 22:33:58 +0900

    BasicLecture46

--- Qthread_3.py
+++ Qthread_3.py
@@ -50,6 +50,28 @@ class Thread3(QThread):
         ###### 실시간 슬롯 (데이터를 받아오는 슬롯을 설정한다)
         self.k.kiwoom.OnReceiveRealData.connect(self.realdata_slot)   # 실시간 데이터를 받아오는 곳
     
+
+    def realdata_slot(self, sCode, sRealType, sRealData):       # 실시간으로 서버에서 데이터들이 날아옴
+
+        if sRealType == "장시작시간":
+            fid = self.realType.REALTYPE[sRealType]['장운영구분']
+
+            # 실시간 시세 데이터 수신 이벤트인 OnReceiveRealData() 가 발생될 때 실시간 데이터를 가져오는 함수
+            value = self.k.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, fid)
+
+            if value == '0':
+                print("장 시작 전")
+            
+            elif value == '3':
+                print("장 중")
+            
+            elif value == '2':
+                print("장 종료, 동시호가")
+            
+            elif value == '4':
+                print("장 마감")
+
+
     def Load_code(self):
 
         if os.path.exists("dist/Selected_code.txt"):
