334aa5aac6ae38067230645b8e0f2540bc363b97
Jaesung_Kim <kisbi3@naver.com>
2023-10-30 23:38:21 +0900

    BasicLecture - Ending

--- Qthread_3.py
+++ Qthread_3.py
@@ -54,7 +54,38 @@ class Thread3(QThread):
 
         ###### 실시간 슬롯 (데이터를 받아오는 슬롯을 설정한다)
         self.k.kiwoom.OnReceiveRealData.connect(self.realdata_slot)   # 실시간 데이터를 받아오는 곳
-    
+
+
+    def Load_code(self):
+
+        if os.path.exists("dist/Selected_code.txt"):
+            f = open("dist/Selected_code.txt", "r", encoding = "utf8")
+            lines = f.readlines()                       # 여러 종목이 저장되어 있다면 모든 항목을 가져온다.
+            screen = 4000
+            
+            for line in lines:
+                if line != "":
+                    ls = line.split("\t")
+                    t_code = ls[0]
+                    t_name = ls[1]
+                    curren_price = ls[2]
+                    dept = ls[3]
+                    mesu = ls[4]
+                    n_o_stock = ls[5]
+                    profit =ls[6]
+                    loss = ls[7].split("\n")[0]
+
+                    self.k.portfolio_stock_dict.update({t_code: {"종목명": t_name}})        # dictionary에 종목 추가
+                    self.k.portfolio_stock_dict[t_code].update({"현재가": int(curren_price)})
+                    self.k.portfolio_stock_dict[t_code].update({"신용비율": dept})
+                    self.k.portfolio_stock_dict[t_code].update({"매수가": int(mesu)})
+                    self.k.portfolio_stock_dict[t_code].update({"매수수량": int(n_o_stock)})
+                    self.k.portfolio_stock_dict[t_code].update({"익절가": int(profit)})
+                    self.k.portfolio_stock_dict[t_code].update({"손절가": int(loss)})
+                    self.k.portfolio_stock_dict[t_code].update({"주문용스크린번호": screen})  # 아래 내용을 업데이트
+                    screen += 1                                                             # 종목마다 스크린 번호를 다르게 하기
+            
+            f.close()
 
     def realdata_slot(self, sCode, sRealType, sRealData):       # 실시간으로 서버에서 데이터들이 날아옴
 
@@ -357,38 +388,6 @@ class Thread3(QThread):
                                 print("재매수 주문 전달 실패")
 
 
-
-
-    def Load_code(self):
-
-        if os.path.exists("dist/Selected_code.txt"):
-            f = open("dist/Selected_code.txt", "r", encoding = "utf8")
-            lines = f.readlines()                       # 여러 종목이 저장되어 있다면 모든 항목을 가져온다.
-            screen = 4000
-            
-            for line in lines:
-                if line != "":
-                    ls = line.split("\t")
-                    t_code = ls[0]
-                    t_name = ls[1]
-                    curren_price = ls[2]
-                    dept = ls[3]
-                    mesu = ls[4]
-                    n_o_stock = ls[5]
-                    profit =ls[6]
-                    loss = ls[7].split("\n")[0]
-
-                    self.k.portfolio_stock_dict.update({t_code: {"종목명": t_name}})        # dictionary에 종목 추가
-                    self.k.portfolio_stock_dict[t_code].update({"현재가": int(curren_price)})
-                    self.k.portfolio_stock_dict[t_code].update({"신용비율": dept})
-                    self.k.portfolio_stock_dict[t_code].update({"매수가": int(mesu)})
-                    self.k.portfolio_stock_dict[t_code].update({"매수수량": int(n_o_stock)})
-                    self.k.portfolio_stock_dict[t_code].update({"익절가": int(profit)})
-                    self.k.portfolio_stock_dict[t_code].update({"손절가": int(loss)})
-                    self.k.portfolio_stock_dict[t_code].update({"주문용스크린번호": screen})  # 아래 내용을 업데이트
-                    screen += 1                                                             # 종목마다 스크린 번호를 다르게 하기
-            
-            f.close()
 
 
     def chejan_slot(self, sGubun, nItemCnt, sFIdList):   # 주문전송 후 주문접수, 체결통보, 잔고통보를 수신
